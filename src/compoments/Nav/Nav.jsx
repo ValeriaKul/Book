60ac3b554a52e8122ddf5f51f5185aee9bf44252
ValeriaKul <lera.dribnyuk@mail.ru>
2023-09-03 00:46:05 +0300

    changed links

--- src/compoments/Nav/Nav.jsx
+++ src/compoments/Nav/Nav.jsx
@@ -4,10 +4,10 @@ import { NavLink } from "react-router-dom";
 
 export default function Nav() {
   const links = [
-    { id: 1, label: "Home", to: "/book-react/" },
-    { id: 2, label: "Author", to: "/book-react/author" },
-    { id: 3, label: "Companies", to: "/book-react/companies" },
-    { id: 4, label: "Articles", to: "/book-react/articles" },
+    { id: 1, label: "Home", to: "/book/" },
+    { id: 2, label: "Author", to: "/book/author" },
+    { id: 3, label: "Companies", to: "/book/companies" },
+    { id: 4, label: "Articles", to: "/book/articles" },
   ];
   const isActive = ({ isActive }) => (isActive ? s.active : "");
   return (
